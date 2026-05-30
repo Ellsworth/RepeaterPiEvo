0549206701ba12c7db3273983b56fb2225b9e962
Erich Ellsworth <erich@kg5key.com>
2026-05-29 21:39:24 -0500

    switch nix-shell to flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "RepeaterPi Evo Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        # Customized Rust toolchain with cross-compilation target added
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "arm-unknown-linux-gnueabihf" ];
+          extensions = [ "clippy" ];
+        };
+
+        # ARMv7 hard float cross GCC from Nixpkgs
+        crossCC = pkgs.pkgsCross.armv7l-hf-multiplatform.stdenv.cc;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.just
+            pkgs.gcc-arm-embedded
+            crossCC
+          ];
+
+          # Environment variables for Cargo to use the correct linker
+          CARGO_TARGET_ARM_UNKNOWN_LINUX_GNUEABIHF_LINKER = "armv7l-unknown-linux-gnueabihf-cc";
+          CC_arm_unknown_linux_gnueabihf = "armv7l-unknown-linux-gnueabihf-cc";
+        };
+      }
+    );
+}
--- justfile
+++ justfile
@@ -2,6 +2,10 @@
 default:
     @just pi3
 
-# Build the project for the aarch64-unknown-linux-musl target in release mode
+# Build the project for the arm-unknown-linux-gnueabihf target in release mode
 pi3:
-    cross build --release --target=arm-unknown-linux-gnueabihf
+    cargo build --release --target=arm-unknown-linux-gnueabihf
+
+# Run clippy on the codebase
+clippy:
+    cargo clippy --all-targets -- -D warnings
